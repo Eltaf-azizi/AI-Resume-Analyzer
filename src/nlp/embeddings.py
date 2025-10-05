4664f6901a08e5646a9bb724bc48ee598d4992c2
Eltaf-azizi <iltfhuss@gmail.com>
2025-10-05 21:16:26 +0500

    working on the transform and fit transform

--- src/nlp/embeddings.py
+++ src/nlp/embeddings.py
@@ -17,3 +17,23 @@ class EmbeddingModel:
         self.backend = backend
         self.model_name = model_name
         self.model = None
+        
+        if backend == "sentence_transformers" and HAVE_SBER:
+            self.model = SentenceTransformer(model_name)
+        else:
+            self.vectorizer = TfidfVectorizer(stop_words="english", max_features=10000)
+
+
+    def fit_transform(self, texts: List[str]):
+        if self.backend == "sentence_transformers" and self.model:
+            return self.model.encode(texts, convert_to_numpy=True, show_progress_bar=False)
+        else:
+            return self.vectorizer.fit_transform(texts).toarray()
+
+
+    def transform(self, texts: List[str]):
+        if self.backend == "sentence_transformers" and self.model:
+            return self.model.encode(texts, convert_to_numpy=True, show_progress_bar=False)
+        else:
+            return self.vectorizer.transform(texts).toarray()
+
