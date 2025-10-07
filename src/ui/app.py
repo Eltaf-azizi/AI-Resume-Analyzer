04132c39cbfbc8d74c70eeb6a29c219cfdf8e7fb
Eltaf-azizi <iltfhuss@gmail.com>
2025-10-07 21:45:48 +0500

    working to save to temp file

--- src/ui/app.py
+++ src/ui/app.py
@@ -13,3 +13,19 @@ st.set_page_config(page_title="AI Resume Analyzer", layout="centered")
 
 st.title("AI Resume Analyzer")
 st.write("Upload a resume (PDF, DOCX, TXT) and get skills and job role suggestions.")
+
+
+
+uploaded = st.file_uploader("Upload resume", type=["pdf", "docx", "txt"])
+
+if uploaded:
+    with st.spinner("Uploading and analyzing..."):
+
+        # save to temp file
+        suffix = "." + uploaded.name.split(".")[-1]
+
+        with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
+            tmp.write(uploaded.getbuffer())
+            tmp_path = tmp.name
+        files = {"file": open(tmp_path, "rb")}
+
